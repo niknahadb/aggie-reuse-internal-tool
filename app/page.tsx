db62bd77021a0575d647d0fc584567e57b7a3a49
Borna <niknahad.borna@gmail.com>
2024-10-22 21:08:52 -0700

    turned it into JSON format

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,8 @@
 import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
-import { fetchTestData } from "./fetchTestData";
 
-export default async function Page() {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
+export default async function Notes() {
+  const supabase = createClient();
+  const { data: test } = await supabase.from("test").select();
 
-  const { data: todos } = await supabase.from("todos").select();
-  const testData = await fetchTestData();
-
-  return (
-    <>
-      <ul>
-        {todos?.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
-      <h2>Test Data:</h2>
-      <ul>
-        {testData?.map((item) => (
-          <li key={item.id}>{JSON.stringify(item)}</li>
-        ))}
-      </ul>
-    </>
-  );
+  return <pre>{JSON.stringify(test, null, 2)}</pre>;
 }
